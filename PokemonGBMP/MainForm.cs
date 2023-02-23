c9b2a5ec32c350836ddd6ccb86195b023d3b2798
Cardinal <alessandro.benvenuti09@gmail.com>
2023-02-23 16:38:39 -0500

    Network code moved to a new class

--- PokemonGBMP/MainForm.cs
+++ PokemonGBMP/MainForm.cs
@@ -51,76 +51,7 @@ namespace PokemonGBMP
             mem.OpenProcess(27752); //27752 14328
         }
 
-        public void BackGroundWorker()
-        {
-            // Inicializar el objeto BackgroundWorker
-            backgroundWorker = new BackgroundWorker();
-            if(isHost)
-                backgroundWorker.DoWork += new DoWorkEventHandler(Host);
-            else
-                backgroundWorker.DoWork += new DoWorkEventHandler(Client);
 
-            // Iniciar el BackgroundWorker
-            backgroundWorker.RunWorkerAsync();
-        }
-
-
-        public void Client(object sender, DoWorkEventArgs e)
-        {
-            client = new TcpClient();
-            client.Connect("127.0.0.1", 12345);
-            stream = client.GetStream();
-            isConnected = true;
-            Listen();
-        }
-
-
-        public void Host(object sender, DoWorkEventArgs e)
-        {
-            host = new TcpListener(IPAddress.Any, 12345);
-            host.Start();
-
-            client = host.AcceptTcpClient();
-            while(stream == null)
-                stream = client.GetStream();
-            isConnected = true;
-            Listen();
-        }
-
-
-        private void Listen()
-        {
-            while(true)
-            {
-                byte[] buffer = new byte[1024];
-
-                int bytes = stream.Read(buffer, 0, buffer.Length);
-
-                string msg = Encoding.ASCII.GetString(buffer, 0, bytes);
-
-                secondaryRelXPos = Int32.Parse(msg);
-
-                if (label1.InvokeRequired)
-                {
-                    label1.Invoke((MethodInvoker)delegate
-                    {
-                        label1.Text = msg;
-                    });
-                }
-                else
-                {
-                    label1.Text = msg;
-                }
-            }
-
-        }
-
-        public void SendSocket(string msg)
-        {
-            byte[] buffer = Encoding.ASCII.GetBytes(msg);
-            stream.Write(buffer, 0, buffer.Length);
-
-        }
 
 
         private void connectBtm_Click(object sender, EventArgs e)
