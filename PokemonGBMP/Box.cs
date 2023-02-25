5c1a7ff2f04d6c18890f5f99850b113e7bcf1be7
Cardinal <alessandro.benvenuti09@gmail.com>
2023-02-25 17:58:59 -0500

    ToBase64

--- PokemonGBMP/Box.cs
+++ PokemonGBMP/Box.cs
@@ -84,7 +84,7 @@ namespace PokemonGBMP
         {
             main.onTrade = true;
             byte[] pkmToChange = main.mem.ReadBytes("visualboyadvance-m.exe+039602E8," + (0xA96 + (33 * slctPkmNum)).ToString("X"), 33);
-            main.SendSocket("T" + BitConverter.ToString(pkmToChange));
+            main.SendSocket("T" + Convert.ToBase64String(pkmToChange));
             MessageBox.Show("Waiting for the other player");
         }
 
